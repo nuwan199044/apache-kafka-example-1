a78f1009afdd01e8304614e72937f360b70329b3
Nuwan <nuwan199044@gmail.com>
2024-06-14 20:00:14 +0530

    basic implementation of avro schema with yml

--- src/main/java/com/myapp/kafka_producer/config/KafkaProducerConfig.java
+++ src/main/java/com/myapp/kafka_producer/config/KafkaProducerConfig.java
@@ -1,44 +1,18 @@
 package com.myapp.kafka_producer.config;
 
-import com.myapp.dto.Employee;
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${topic.name}")
+    private String TOPIC;
+
     @Bean
     public NewTopic createTopic() {
-        return new NewTopic("employeeTopic-avro", 3, (short) 1);
-    }
-
-    @Bean
-    public Map<String,Object> producerConfig() {
-        Map<String,Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        props.put("schema.registry.url", "http://127.0.0.1:8081");
-        return props;
-    }
-
-    @Bean
-    public ProducerFactory<String, Employee> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(producerConfig());
-    }
-
-    @Bean
-    public KafkaTemplate<String,Employee> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+        return new NewTopic(TOPIC, 3, (short) 1);
     }
 }
--- src/main/java/com/myapp/kafka_producer/service/KafkaMessagePublisher.java
+++ src/main/java/com/myapp/kafka_producer/service/KafkaMessagePublisher.java
@@ -3,6 +3,7 @@ package com.myapp.kafka_producer.service;
 import com.myapp.dto.Employee;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
@@ -13,11 +14,14 @@ import java.util.concurrent.CompletableFuture;
 @Service
 @Slf4j
 public class KafkaMessagePublisher {
+
+    @Value("${topic.name}")
+    private String TOPIC;
     @Autowired
     KafkaTemplate<String, Employee> template;
 
     public void sendEventToTopic(Employee employee) {
-        CompletableFuture<SendResult<String, Employee>> customerTopic = template.send("employeeTopic-avro", UUID.randomUUID().toString(), employee);
+        CompletableFuture<SendResult<String, Employee>> customerTopic = template.send(TOPIC, UUID.randomUUID().toString(), employee);
         customerTopic.whenComplete((result, exception)->{
             if (exception == null) {
                 System.out.println("Sent message=[ "+employee.toString()+ "] with offset=[ "+result.getRecordMetadata().offset()+" ]");
